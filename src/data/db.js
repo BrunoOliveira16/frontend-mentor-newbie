25c074d186fc72189958a01a352271ad12e3bc2a
Bruno Oliveira <103857382+BrunoOliveira16@users.noreply.github.com>
2023-04-22 11:17:48 -0300

    :sparkles: feat: Implementado novo projeto concluido

--- src/data/db.js
+++ src/data/db.js
@@ -2,6 +2,28 @@ export default {
     projects : [
         {
             id: 1,
+            name: "Results Summary",
+            image: "./src/assets/images/desktop-preview-06.jpg",
+            tags: [ 
+                {
+                    id: "html",
+                    name: "HTML"
+                },
+                {
+                    id: "sass",
+                    name: "SASS"
+                },
+                {
+                    id: "js",
+                    name: "JAVASCRIPT"
+                },
+             ],
+            description: "Projeto de criação de um componente de summario de resultados com sass, respeitando o layout fornecido e gerando dinamicamente o sumario",
+            url_github: "https://github.com/BrunoOliveira16/frontend-mentor-newbie/tree/main/src/Projects/project-05-Results-summary-component",
+            url_demo: "./src/Projects/project-05-Results-summary-component/index.html"
+        },
+        {
+            id: 2,
             name: "Order Summary",
             image: "./src/assets/images/desktop-preview-05.jpg",
             tags: [ 
@@ -14,12 +36,12 @@ export default {
                     name: "CSS"
                 },
              ],
-            description: "Projeto de desafio do frontend mentor para criação de um componente de order summary centralizado na tela, respeitando o layout fornecido.",
+            description: "Projeto de criação de um componente de sumario de ordem de compra centralizado na tela, respeitando o layout fornecido com html e css.",
             url_github: "https://github.com/BrunoOliveira16/frontend-mentor-newbie/tree/main/src/Projects/project-04-order-summary",
             url_demo: "./src/Projects/project-04-order-summary/index.html"
         },
         {
-            id: 2,
+            id: 3,
             name: "Componente Card NFT",
             image: "./src/assets/images/desktop-preview-04.jpg",
             tags: [ 
@@ -32,12 +54,12 @@ export default {
                     name: "CSS"
                 },
              ],
-            description: "Projeto de desafio do frontend mentor para criação de um componente de card NFT centralizado na tela, respeitando o layout fornecido.",
+            description: "Projeto de criação de um componente de card NFT centralizado na tela, respeitando o layout fornecido, com css e html e interação com elementos do card.",
             url_github: "https://github.com/BrunoOliveira16/frontend-mentor-newbie/tree/main/src/Projects/project-03-nft-preview-card",
             url_demo: "./src/Projects/project-03-nft-preview-card/index.html"
         },
         {
-            id: 3,
+            id: 4,
             name: "Componente Card de Produto",
             image: "./src/assets/images/desktop-preview-03.jpg",
             tags: [ 
@@ -50,12 +72,12 @@ export default {
                     name: "CSS"
                 },
              ],
-            description: "Projeto de desafio do frontend mentor para criação de um componente card de produto, centralizado na tela, respeitando o layout fornecido.",
+            description: "Projeto de criação de um componente de card de produto, centralizado na tela, respeitando o layout fornecido com css e html e interação com elementos do card.",
             url_github: "https://github.com/BrunoOliveira16/frontend-mentor-newbie/tree/main/src/Projects/project-02-card-component",
             url_demo: "./src/Projects/project-02-card-component/index.html"
         },
         {
-            id: 4,
+            id: 5,
             name: "Componente QR Code",
             image: "./src/assets/images/desktop-preview-01.jpg",
             tags: [ 
